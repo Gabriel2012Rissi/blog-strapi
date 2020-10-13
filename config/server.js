81fc7bfa35a854cc5c3399d8373607ad6e455879
Gabriel Rissi <gabriel2012rissi@gmail.com>
2020-10-12 21:00:48 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9f5dec6b7f95736ada712bce3ad896b3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'edc238d4-4084-436d-b55c-bf49e27e7aeb'
+};
